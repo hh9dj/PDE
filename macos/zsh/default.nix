1cafb4a7b9bc66a7c5c19fd37e681cd1a9cbe9ef
khalil hadji <h-adji_tech@proton.me>
2026-09-11 15:19:43 +0200

    wifi cmd alias

--- macos/zsh/default.nix
+++ macos/zsh/default.nix
@@ -3,5 +3,6 @@
   programs.zsh.shellAliases = {
     nix_switch = "sudo darwin-rebuild switch --flake ~/PDE#macbook";
     nh_switch = "nh darwin switch";
+    refresh_wifi = "networksetup -setairportpower en0 off && networksetup -setairportpower en0 on";
   };
 }
